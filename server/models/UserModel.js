8ec3b122ef207f78606e9e1a2c275eb964b90a5f
Dhruv Arora <101944782+itsdhruvarora@users.noreply.github.com>
2024-02-21 10:22:36 +0530

    updated usermodel

--- server/models/UserModel.js
+++ server/models/UserModel.js
@@ -14,6 +14,8 @@ const userSchema = new mongoose.Schema(
     yearsOfExperience: Number,
     organization: String,
     role: String,
+    jobsapplied: [],
+    socialmedia: [],
   },
   { timestamps: true }
 );
